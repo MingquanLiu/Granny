22034196055ace8b487e11b7bf73fbe641c8e11f
MingquanLiu <mikelmq99@gmail.com>
2018-01-13 08:35:29 -0500

    add a button

--- app/src/main/java/com/example/mikel/granny/MainActivity.java
+++ app/src/main/java/com/example/mikel/granny/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.mikel.granny;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    final String tag = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Log.e(tag,"I am here");
+            }
+        });
+
     }
 }
