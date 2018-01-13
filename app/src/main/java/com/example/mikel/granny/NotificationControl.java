1942021068dda9206180dec5db194af570c8eb86
ywang20 <ywang20@wpi.edu>
2018-01-13 14:24:46 -0500

    notif

--- app/src/main/java/com/example/mikel/granny/NotificationControl.java
+++ app/src/main/java/com/example/mikel/granny/NotificationControl.java
@@ -2,17 +2,13 @@ package com.example.mikel.granny;
 
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-
-public class NotificationController extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+public class NotificationControl extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
@@ -21,13 +17,8 @@ public class NotificationController extends AppCompatActivity {
         Intent intent = getIntent();
         String title= intent.getStringExtra("title");
         String context = intent.getStringExtra("text");
-        Notification noti = new Notification.Builder(getApplicationContext())
-                .setContentText(context)
-                .setContentTitle(title)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .build();
+        Notification noti = new Notification.Builder(getApplicationContext()).setContentText(context).setContentTitle(title).setSmallIcon(R.drawable.ic_launcher_foreground).build();
         NM.notify(0, noti);
 
     }
-
 }
